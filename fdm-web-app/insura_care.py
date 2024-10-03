f699eeaeead20e01fa7d72e590cef301ebab23da
Azri Muhsin <azrimuhsin246@gmail.com>
2024-10-04 00:18:04 +0530

    dependancy issue

--- fdm-web-app/insura_care.py
+++ fdm-web-app/insura_care.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import pickle 
-from sklearn.naive_bayes import GaussianNB
+
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
